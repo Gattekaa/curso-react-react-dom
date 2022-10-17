846cb37a8b2301ea37d9f767dd54848d4b505983
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-17 15:55:47 -0300

    Finished the route system and added the NotFound page in case the user tries to access a route that has not been configured

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,17 +3,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
  
 import Home from "../../views/examples/Home";
-//import Param from "../../views/examples/Param";
-//import About from "../../views/examples/About";
-//import NotFound from "../../views/examples/NotFound";
+import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
  
 const Content = props => (
   <main className="Content">
-    {/* Toda essa parte está diferente da do professor, analisem as diferenças */}
     <Routes>
+      <Route path="/about" exact element={<About />} />
+      <Route path="/param/:id" exact element={<Param />} />
       <Route path="/" exact element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+
     </Routes>
-    {/* --------------------------------------------------- */}
   </main>
 );
  
